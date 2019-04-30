acbf7b7263929637b5b40bf92530aaa746c7663d
jennifer <peters84@seattleu.edu>
2019-04-29 17:22:50 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -5,6 +5,91 @@
       "Product_Name": "Screw",
       "Product_Price": "$12",
       "Product_Supplier": "Averson"
-    }
+    },
+     {
+      "Product_Number": "464747464647",
+      "Product_Name": "Screw",
+      "Product_Price": "$12",
+      "Product_Supplier": "Averson"
+    },
+     {
+      "Product_Number": "464747464647",
+      "Product_Name": "Screw",
+      "Product_Price": "$12",
+      "Product_Supplier": "Averson"
+    },
+     {
+      "Product_Number": "464747464647",
+      "Product_Name": "Screw",
+      "Product_Price": "$12",
+      "Product_Supplier": "Averson"
+    },
+     {
+      "Product_Number": "464747464647",
+      "Product_Name": "Screw",
+      "Product_Price": "$12",
+      "Product_Supplier": "Averson"
+    },
+     {
+      "Product_Number": "464747464647",
+      "Product_Name": "Screw",
+      "Product_Price": "$12",
+      "Product_Supplier": "Averson"
+    },
+     {
+      "Product_Number": "464747464647",
+      "Product_Name": "Screw",
+      "Product_Price": "$12",
+      "Product_Supplier": "Averson"
+    },
+     {
+      "Product_Number": "464747464647",
+      "Product_Name": "Screw",
+      "Product_Price": "$12",
+      "Product_Supplier": "Averson"
+    },
+     {
+      "Product_Number": "464747464647",
+      "Product_Name": "Screw",
+      "Product_Price": "$12",
+      "Product_Supplier": "Averson"
+    },
+     {
+      "Product_Number": "464747464647",
+      "Product_Name": "Screw",
+      "Product_Price": "$12",
+      "Product_Supplier": "Averson"
+    },
+     {
+      "Product_Number": "464747464647",
+      "Product_Name": "Screw",
+      "Product_Price": "$12",
+      "Product_Supplier": "Averson"
+    },
+     {
+      "Product_Number": "464747464647",
+      "Product_Name": "Screw",
+      "Product_Price": "$12",
+      "Product_Supplier": "Averson"
+    },
+     {
+      "Product_Number": "464747464647",
+      "Product_Name": "Screw",
+      "Product_Price": "$12",
+      "Product_Supplier": "Averson"
+    },
+     {
+      "Product_Number": "464747464647",
+      "Product_Name": "Screw",
+      "Product_Price": "$12",
+      "Product_Supplier": "Averson"
+    },
+     {
+      "Product_Number": "464747464647",
+      "Product_Name": "Screw",
+      "Product_Price": "$12",
+      "Product_Supplier": "Averson"
+    },
+    
     ]
   }
