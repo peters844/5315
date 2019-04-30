7682a084d3ef43f7bea5d672fd76c7821dcbec1c
jennifer <peters84@seattleu.edu>
2019-04-29 17:30:21 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -1,93 +1,99 @@
 {
   "data": [
     {
-      "Product_Number": "464747464647",
+      "Product_Number": "100",
       "Product_Name": "Screw",
       "Product_Price": "$12",
       "Product_Supplier": "Averson"
     },
      {
-      "Product_Number": "464747464647",
-      "Product_Name": "Screw",
-      "Product_Price": "$12",
+      "Product_Number": "101",
+      "Product_Name": "Binder",
+      "Product_Price": "$7",
+      "Product_Supplier": "Wiley"
+    },
+     {
+      "Product_Number": "102",
+      "Product_Name": "Paper",
+      "Product_Price": "$4",
+      "Product_Supplier": "Wiley"
+    },
+     {
+      "Product_Number": "103",
+      "Product_Name": "Binder",
+      "Product_Price": "$6",
+      "Product_Supplier": "Peterson"
+    },
+     {
+      "Product_Number": "104",
+      "Product_Name": "Ruler",
+      "Product_Price": "$1",
       "Product_Supplier": "Averson"
     },
      {
-      "Product_Number": "464747464647",
-      "Product_Name": "Screw",
-      "Product_Price": "$12",
+      "Product_Number": "105",
+      "Product_Name": "Stapler",
+      "Product_Price": "$3",
+      "Product_Supplier": "Wiley"
+    },
+     {
+      "Product_Number": "106",
+      "Product_Name": "Pencil",
+      "Product_Price": "$2",
       "Product_Supplier": "Averson"
     },
      {
-      "Product_Number": "464747464647",
-      "Product_Name": "Screw",
-      "Product_Price": "$12",
+      "Product_Number": "107",
+      "Product_Name": "Pen",
+      "Product_Price": "$3",
       "Product_Supplier": "Averson"
     },
      {
-      "Product_Number": "464747464647",
-      "Product_Name": "Screw",
-      "Product_Price": "$12",
+      "Product_Number": "108",
+      "Product_Name": "Hammer",
+      "Product_Price": "$8",
       "Product_Supplier": "Averson"
     },
      {
-      "Product_Number": "464747464647",
-      "Product_Name": "Screw",
-      "Product_Price": "$12",
+      "Product_Number": "109",
+      "Product_Name": "Nails",
+      "Product_Price": "$2",
+      "Product_Supplier": "Peterson"
+    },
+     {
+      "Product_Number": "110",
+      "Product_Name": "Chair",
+      "Product_Price": "$120",
       "Product_Supplier": "Averson"
     },
      {
-      "Product_Number": "464747464647",
-      "Product_Name": "Screw",
-      "Product_Price": "$12",
+      "Product_Number": "111",
+      "Product_Name": "Table",
+      "Product_Price": "$200",
+      "Product_Supplier": "Milford"
+    },
+     {
+      "Product_Number": "112",
+      "Product_Name": "Storage",
+      "Product_Price": "$50",
       "Product_Supplier": "Averson"
     },
      {
-      "Product_Number": "464747464647",
-      "Product_Name": "Screw",
-      "Product_Price": "$12",
-      "Product_Supplier": "Averson"
+      "Product_Number": "113",
+      "Product_Name": "Computer",
+      "Product_Price": "$1200",
+      "Product_Supplier": "Apple"
     },
      {
-      "Product_Number": "464747464647",
-      "Product_Name": "Screw",
-      "Product_Price": "$12",
-      "Product_Supplier": "Averson"
+      "Product_Number": "114",
+      "Product_Name": "Keyboard",
+      "Product_Price": "$40",
+      "Product_Supplier": "Dell"
     },
      {
-      "Product_Number": "464747464647",
-      "Product_Name": "Screw",
-      "Product_Price": "$12",
-      "Product_Supplier": "Averson"
-    },
-     {
-      "Product_Number": "464747464647",
-      "Product_Name": "Screw",
-      "Product_Price": "$12",
-      "Product_Supplier": "Averson"
-    },
-     {
-      "Product_Number": "464747464647",
-      "Product_Name": "Screw",
-      "Product_Price": "$12",
-      "Product_Supplier": "Averson"
-    },
-     {
-      "Product_Number": "464747464647",
-      "Product_Name": "Screw",
-      "Product_Price": "$12",
-      "Product_Supplier": "Averson"
-    },
-     {
-      "Product_Number": "464747464647",
-      "Product_Name": "Screw",
-      "Product_Price": "$12",
-      "Product_Supplier": "Averson"
-    },
-     {
-      "Product_Number": "464747464647",
-      "Product_Name": "Screw",
-      "Product_Price": "$12",
+      "Product_Number": "115",
+      "Product_Name": "Mouse",
+      "Product_Price": "$20",
       "Product_Supplier": "Averson"
     }
     ]
